5a8bcc96783430d0a8d67a443cc69c18dc8c5b09
AlexeyDanilov <al.s.danilov@gmail.com>
2018-01-29 13:30:10 +0300

    все блюда + footer только разметка

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ $( document ).ready(function(){
 	if($(window).innerWidth() < 768){
 			var mySwiper = new Swiper('.all_production', {
 		    speed: 400,
-		    spaceBetween: 100
+		    spaceBetween: 100,
+		    autoHeight: true,
+		    pagination: {
+        		el: '.swiper-pagination',
+        		clickable: true,
+      		},
+      		navigation: {
+        		nextEl: '.swiper-button-next',
+        		prevEl: '.swiper-button-prev',
+      		},
 		});
 	}
 });
